16b40f3e19d51f4761d6a00f7c9527dfa5f871a4
knutbo <knutbo@ea201a5e-fe21-0410-8215-f94e07e9a8b2>
2006-11-20 22:42:06 +0000

    inventory work.

---  vikingmudapplet --username knutbo/vikingclient/no/knubo/mud/Inventory.java
+++  vikingmudapplet --username knutbo/vikingclient/no/knubo/mud/Inventory.java
@@ -1,5 +1,9 @@
 package no.knubo.mud;
 
+import java.awt.Font;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.PrintStream;
@@ -10,6 +14,7 @@ import java.util.LinkedList;
 import java.util.List;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JTree;
 import javax.swing.event.TreeExpansionEvent;
 import javax.swing.event.TreeExpansionListener;
@@ -40,6 +45,20 @@ public class Inventory extends JFrame
 
 	private HashSet bagsOpen = new HashSet();
 
+	private JLabel nameLabel;
+
+	private JLabel lvlAmount;
+
+	private JLabel expAmount;
+
+	private JLabel strAmount;
+
+	private JLabel dexAmount;
+
+	private JLabel intAmount;
+
+	private JLabel conAmount;
+
 	Inventory() {
 		tree = new JTree();
 		tree.setModel(this);
@@ -47,7 +66,76 @@ public class Inventory extends JFrame
 		tree.setDoubleBuffered(true);
 		tree.setRootVisible(false);
 		tree.addTreeExpansionListener(this);
+
+		nameLabel = new JLabel();
+		nameLabel.setFont(new Font("Lucida",Font.BOLD, 18));
+		JLabel inventoryLabel = new JLabel("Inventory");
+		JLabel statsLabel = new JLabel("Stats");
+
+		JLabel lvlLabel = new JLabel("Level");
+		JLabel expLabel = new JLabel("EXP");
+		JLabel strLabel = new JLabel("STR");
+		JLabel dexLabel = new JLabel("DEX");
+		JLabel conLabel = new JLabel("CON");
+		JLabel intLabel = new JLabel("INT");
+
+		lvlAmount = new JLabel();
+		expAmount = new JLabel();
+		strAmount = new JLabel();
+		dexAmount = new JLabel();
+		intAmount = new JLabel();
+		conAmount = new JLabel();
+
+		GridBagLayout gbl = new GridBagLayout();
+		setLayout(gbl);
+		GridBagConstraints displayConstraints = new GridBagConstraints();
+
+		displayConstraints.gridx = 0;
+		displayConstraints.gridy = 0;
+		displayConstraints.anchor = GridBagConstraints.CENTER;
+		displayConstraints.gridwidth = 3;
+		gbl.setConstraints(nameLabel, displayConstraints);
+		getContentPane().add(nameLabel);
+
+		addLabel(inventoryLabel, gbl, displayConstraints, 2, 1);
+		displayConstraints.insets = new Insets(0,0,0,20);
+		addLabel(statsLabel, gbl, displayConstraints, 0, 1);
+		addLabel(lvlLabel, gbl, displayConstraints, 0, 2);
+		addLabel(expLabel, gbl, displayConstraints, 0, 3);
+		addLabel(strLabel, gbl, displayConstraints, 0, 4);
+		addLabel(dexLabel, gbl, displayConstraints, 0, 5);
+		addLabel(conLabel, gbl, displayConstraints, 0, 6);
+		addLabel(intLabel, gbl, displayConstraints, 0, 7);
+
+		addLabel(lvlAmount, gbl, displayConstraints, 1, 2);
+		addLabel(expAmount, gbl, displayConstraints, 1, 3);
+		addLabel(strAmount, gbl, displayConstraints, 1, 4);
+		addLabel(dexAmount, gbl, displayConstraints, 1, 5);
+		addLabel(conAmount, gbl, displayConstraints, 1, 6);
+		addLabel(intAmount, gbl, displayConstraints, 1, 7);
+
+		displayConstraints.gridx = 2;
+		displayConstraints.gridy = 2;
+		displayConstraints.gridwidth = 1;
+		displayConstraints.gridheight = 6;
+		displayConstraints.anchor = GridBagConstraints.NORTHWEST;
+		displayConstraints.fill = GridBagConstraints.BOTH;
+		displayConstraints.weightx = 1;
+		displayConstraints.weighty = 1;
+		gbl.setConstraints(tree, displayConstraints);
 		getContentPane().add(tree);
+		
+		setSize(400,400);
+	}
+
+	private void addLabel(JLabel label, GridBagLayout gbl,
+			GridBagConstraints displayConstraints, int x, int y) {
+		displayConstraints.gridx = x;
+		displayConstraints.gridy = y;
+		displayConstraints.anchor = GridBagConstraints.NORTHWEST;
+		displayConstraints.gridwidth = 1;
+		gbl.setConstraints(label, displayConstraints);
+		getContentPane().add(label);
 	}
 
 	public String readInventory(InputStream vikingIn, PrintStream vikingOut)
@@ -100,13 +188,22 @@ public class Inventory extends JFrame
 
 		inventory.clear();
 
+		boolean pickedFirst = false;
+
 		Container container = null;
-		for (int i = 0; i < lines.length; i++) {
+		for (int i = 1; i < lines.length; i++) {
 			String line = lines[i];
 
 			if (line.length() == 0) {
 				continue;
 			}
+			
+			if(!pickedFirst) {
+				pickedFirst = true;
+				setStats(line);
+				continue;
+			}
+			/* First line are player stats */
 
 			boolean inContainer = line.startsWith(">");
 			if (inContainer) {
@@ -135,6 +232,25 @@ public class Inventory extends JFrame
 			}
 		}
 	}
+	private void setStats(String string) {
+		String[] stats = string.split(ESC);
+
+		nameLabel.setText(stats[0]);
+		lvlAmount.setText(stats[1]);
+		expAmount.setText(stats[2]);
+		// money 3
+		// bank 4
+		strAmount.setText(Integer.parseInt(stats[5])
+				+ Integer.parseInt(stats[6]) + "(" + stats[5] + ")");
+		dexAmount.setText(Integer.parseInt(stats[7])
+				+ Integer.parseInt(stats[8]) + "(" + stats[7] + ")");
+		intAmount.setText(Integer.parseInt(stats[9])
+				+ Integer.parseInt(stats[10]) + "(" + stats[9] + ")");
+		conAmount.setText(Integer.parseInt(stats[11])
+				+ Integer.parseInt(stats[12]) + "(" + stats[11] + ")");
+
+	}
+
 	private Item createItem(String[] data) {
 		int id = Integer.parseInt(data[0]);
 		boolean tagged = data[1].charAt(0) == '1';
@@ -143,7 +259,7 @@ public class Inventory extends JFrame
 		int wornOut = Integer.parseInt(data[3]);
 		boolean worn = false;
 		char wield = 0;
-		
+
 		String armourType = null;
 
 		switch (type) {
@@ -269,6 +385,5 @@ public class Inventory extends JFrame
 		}
 
 	}
-
 
 }
